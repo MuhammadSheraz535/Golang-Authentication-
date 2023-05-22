6d07c0fcf3c4fc11e3096c15c5f4b04bc8077008
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-05-22 09:12:26 +0500

    Add login function for services

--- service/registration.go
+++ service/registration.go
@@ -3,18 +3,14 @@ package service
 import (
 	"errors"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/MuhammadSheraz535/golang-authentication/controller"
 	"github.com/MuhammadSheraz535/golang-authentication/database"
 	log "github.com/MuhammadSheraz535/golang-authentication/logger"
 	"github.com/MuhammadSheraz535/golang-authentication/models"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
-	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -162,84 +158,4 @@ func (s *SignupService) DeleteRegisterUser(c *gin.Context) {
 
 	c.Status(http.StatusNoContent)
 
-}
-
-// Login
-
-func (s *SignupService) Login(c *gin.Context) {
-
-	log.Info("Initializing Login User handler function...")
-
-	// binding user
-	var loginuser *models.Login
-
-	if err := c.ShouldBindJSON(&loginuser); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	err := loginuser.Validate()
-	if err != nil {
-		errs, ok := controller.ErrValidationSlice(err)
-		if !ok {
-			log.Error(err.Error())
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		log.Error(err.Error())
-		if len(errs) > 1 {
-			c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
-		} else {
-			c.JSON(http.StatusBadRequest, gin.H{"error": errs[0]})
-		}
-		return
-	}
-
-	//check user by email
-	var user *models.SignUp
-	if s.Db.Model(models.SignUp{}).Where("email = ?", loginuser.Email).Find(&user).RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email or password"})
-		return
-	}
-	// compare user password  with requested user password
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginuser.Password))
-
-	if err != nil {
-
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email or password"})
-		return
-
-	}
-
-	//Generate a jwt token
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": user.ID,
-		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
-	})
-
-	// Sign and get the complete encoded token as a string using the secret
-	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create token"})
-		return
-	}
-	//set it cookie
-	c.SetSameSite(http.SameSiteLaxMode)
-	//set cookie and send it back
-	c.SetCookie("Authorization", tokenString, 3600*24*1, "", "", false, true)
-
-	c.JSON(http.StatusOK, gin.H{"message": "login"})
-}
-
-//validate user credentials
-
-func (s *SignupService) Validate(c *gin.Context) {
-	user, ok := c.Get("user")
-	if !ok {
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"message": user})
 }
